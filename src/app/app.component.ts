cc13f19481d69003edea799d623d854a2272ee66
Banjos <brunosilveiradosanjos@gmail.com>
2025-03-05 11:25:42 -0300

    feat: add workspace-financeira-scd- as a submodule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { ButtonComponentLib } from 'lib-turbo-components-scd'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonComponentLib],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
