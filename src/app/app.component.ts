d7164938f1849793aaa5bf7d1fd38abd482d1b42
Banjos <brunosilveiradosanjos@gmail.com>
2025-03-05 22:18:53 -0300

    feat: setup library workspace-financeira-scd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
 import { ButtonComponentLib } from 'lib-turbo-components-scd'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponentLib],
+  imports: [ButtonComponentLib],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
